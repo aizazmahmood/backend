b9de506285660ee252d2a57110d60912f0c86cc9
muhammadmunafkhan <167846557+muhammadmunafkhan@users.noreply.github.com>
2025-12-11 21:39:47 +0500

    Cors Policy Update

--- backend/Program.cs
+++ backend/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(corsPolicy, policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5174", "http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
